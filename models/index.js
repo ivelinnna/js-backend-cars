8b0fcfee9ca16a4498e3666efebaeeb3350ea0e5
Ivelina Kotseva <ivellinna.kotseva@gmail.com>
2022-01-31 13:53:25 +0200

    upgraded car service

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const cars = require('../services/cars.js');
 
-require('./Car');
+ require('./Car');
 
 const connectionString = 'mongodb://localhost:27017/carbicle';
 
